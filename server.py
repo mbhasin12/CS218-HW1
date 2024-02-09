a63b6e37ad8022788b7b3173034a80a0fe39df43
mbhasin1 <mbhasin@purdue.edu>
2024-02-09 11:36:57 -0800

    added config.yml

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask
 from flask import request
+import os
 
 app = Flask(__name__)
 
@@ -38,6 +39,7 @@ def getWeather():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
